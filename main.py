c2fab3d528e4f69a0a0f00bd0ed9bcfe43fbfb12
Raymondtamtam <118673665+Raymondtamtam@users.noreply.github.com>
2023-03-28 21:58:05 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,129 @@
+import os
+import sys
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+tf.random.set_seed(123)
+#-----------------------------------------------------------------
+import zipfile as z
+
+Dataset = "test-depth"
+
+# with z.ZipFile("./input/"+Dataset+".tar","r") as z:
+#     z.extractall(".")
+
+
+path = "../input/test-depth/val/indoors"
+
+filelist = []
+
+for root, dirs, files in os.walk(path):
+    for file in files:
+#         print(file)
+        filelist.append(os.path.join(root, file))
+
+filelist.sort()
+data = {
+    "image": [x for x in filelist if x.endswith(".png")],
+    "depth": [x for x in filelist if x.endswith("_depth.npy")],
+    "mask": [x for x in filelist if x.endswith("_depth_mask.npy")],
+}
+
+df = pd.DataFrame(data)
+df = df.sample(frac=1, random_state=42)
+
+print(df)
+
+# Preparing hyperparameters----------------------------------------------------------------------------------
+
+HEIGHT = 256
+WIDTH = 256
+LR = 0.0002
+EPOCHS = 30
+BATCH_SIZE = 32
+
+# Building a data pipeline-------------------------------------------------------------------------------
+
+class DataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, data, batch_size=6, dim=(768, 1024), n_channels=3, shuffle=True):
+        """
+        Initialization
+        """
+        self.data = data
+        self.indices = self.data.index.tolist()
+        self.dim = dim
+        self.n_channels = n_channels
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.min_depth = 0.1
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.ceil(len(self.data) / self.batch_size))
+
+    def __getitem__(self, index):
+        if (index + 1) * self.batch_size > len(self.indices):
+            self.batch_size = len(self.indices) - index * self.batch_size
+        # Generate one batch of data
+        # Generate indices of the batch
+        index = self.indices[index * self.batch_size : (index + 1) * self.batch_size]
+        # Find list of IDs
+        batch = [self.indices[k] for k in index]
+        x, y = self.data_generation(batch)
+
+        return x, y
+
+    def on_epoch_end(self):
+
+        """
+        Updates indexes after each epoch
+        """
+        self.index = np.arange(len(self.indices))
+        if self.shuffle == True:
+            np.random.shuffle(self.index)
+
+    def load(self, image_path, depth_map, mask):
+        """Load input and target image."""
+
+        image_ = cv2.imread(image_path)
+        image_ = cv2.cvtColor(image_, cv2.COLOR_BGR2RGB)
+        image_ = cv2.resize(image_, self.dim)
+        image_ = tf.image.convert_image_dtype(image_, tf.float32)
+
+        depth_map = np.load(depth_map).squeeze()
+
+        mask = np.load(mask)
+        mask = mask > 0
+
+        max_depth = min(300, np.percentile(depth_map, 99))
+        depth_map = np.clip(depth_map, self.min_depth, max_depth)
+        depth_map = np.log(depth_map, where=mask)
+
+        depth_map = np.ma.masked_where(~mask, depth_map)
+
+        depth_map = np.clip(depth_map, 0.1, np.log(max_depth))
+        depth_map = cv2.resize(depth_map, self.dim)
+        depth_map = np.expand_dims(depth_map, axis=2)
+        depth_map = tf.image.convert_image_dtype(depth_map, tf.float32)
+
+        return image_, depth_map
+
+    def data_generation(self, batch):
+
+        x = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size, *self.dim, 1))
+
+        for i, batch_id in enumerate(batch):
+            x[i,], y[i,] = self.load(
+                self.data["image"][batch_id],
+                self.data["depth"][batch_id],
+                self.data["mask"][batch_id],
+            )
+
+        return x, y
